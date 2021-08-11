55c66a77b88c76db903606bef167b1629666bd1c
Dlaize <dakshkohli23@gmail.com>
2021-08-11 05:16:48 +0000

    Auto Fixes: Code formatted

--- DaisyX/__init__.py
+++ DaisyX/__init__.py
@@ -19,12 +19,14 @@ import logging
 import spamwatch
 from aiogram import Bot, Dispatcher, types
 from aiogram.bot.api import TELEGRAM_PRODUCTION, TelegramAPIServer
-#from aiogram.contrib.fsm_storage.redis import *
 
 from DaisyX.config import get_bool_key, get_int_key, get_list_key, get_str_key
 from DaisyX.services.telethon import tbot
 from DaisyX.utils.logger import log
 from DaisyX.versions import DAISY_VERSION
+
+# from aiogram.contrib.fsm_storage.redis import *
+
 
 log.info("----------------------")
 log.info("|      Nico Robin      |")
@@ -58,12 +60,12 @@ else:
 
 # AIOGram
 bot = Bot(token=TOKEN, parse_mode=types.ParseMode.HTML, server=server)
-#storage = RedisStorage2(
+# storage = RedisStorage2(
 #    host=get_str_key("REDIS_URI"),
 #    port=get_int_key("REDIS_PORT"),
 #    password=get_str_key("REDIS_PASS"),
-#)
-#dp = Dispatcher(bot, storage=storage)
+# )
+# dp = Dispatcher(bot, storage=storage)
 
 loop = asyncio.get_event_loop()
 SUPPORT_CHAT = get_str_key("SUPPORT_CHAT", required=True)
